2885f085936ec06209880ab22a77b4ee573bf876
Kyle Rader <kylewrader@gmail.com>
2024-01-28 02:27:33 -0800

    get connected to postgres!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use postgres::{tls::TlsConnect, Client, NoTls};
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// Enable verbose output
+    #[clap(long)]
+    verbose: bool,
+
+    /// Database connection URL
+    #[clap(long, env = "DATABASE_URL")]
+    db_url: Option<String>,
+}
+
+fn main() -> anyhow::Result<()> {
+    let Args { verbose, db_url } = Args::parse();
+    println!("Verbose mode is {}", if verbose { "on" } else { "off" });
+
+    let Some(db_url) = db_url else {
+        eprintln!("error: The --db-url option or DATABASE_URL environment variable must be used");
+        std::process::exit(1);
+    };
+
+    println!("Database URL is {}", db_url);
+
+    let mut client = Client::connect(&db_url, NoTls)?;
+
+    let dbs = client.query(
+        "SELECT datname FROM pg_database WHERE datistemplate = false;",
+        &[],
+    )?;
+
+    let dbs = dbs
+        .into_iter()
+        .map(|row| row.get(0))
+        .collect::<Vec<String>>();
+    println!("Databases: {dbs:#?}");
+
+    Ok(())
 }
